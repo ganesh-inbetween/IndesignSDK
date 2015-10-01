7be9c2491b659e9635d7e2deb6ba8b08f03ed4ff
ganesh-inbetween <ganesh@inbetween.co.in>
2015-10-01 20:44:42 +0530

    DTP-1481 C++ Plugin (text selection)
    DTP-1481 #time 5h

--- MyPlugin2/MyPlugin2SelectionObserver.cpp
+++ MyPlugin2/MyPlugin2SelectionObserver.cpp
@@ -5,11 +5,20 @@
 #include "MyPlugin2SelectionObserver.h"
 #include "MyPlugin2ID.h"
 
+#include "ISelectionUtils.h"
 #include "CAlert.h"
-
+#include "Utils.h"
 #include "CObserver.h"
 #include "GenericID.h"
 #include "CPMUnknown.h"
+#include "ITextSelectionSuite.h"
+#include "ITextTarget.h"
+#include "IXMLUtils.h"
+#include "IRangeData.h"
+#include "RangeData.h"
+#include "IIntegratorTarget.h"
+#include "textiterator.h"
+#include "ITextModel.h"
 
 CREATE_PMINTERFACE(MyPlugin2SelectionObserver, kMyPlugin2SelectionObserverImpl)
 
@@ -27,14 +36,52 @@ MyPlugin2SelectionObserver::~MyPlugin2SelectionObserver()
 
 /*
 */
-void MyPlugin2SelectionObserver::HandleSelectionChanged(const ISelectionMessage* message)
+void MyPlugin2SelectionObserver::HandleSelectionChanged(const ISelectionMessage* selectionMessage)
 {
-	CAlert::InformationAlert("Selection changed..");
+	
+	ISelectionManager* iSelectionManager = Utils<ISelectionUtils>()->GetActiveSelection();
+
+	InterfacePtr<ITextSelectionSuite> iTextSelectionSuite(iSelectionManager, UseDefaultIID());
+	if (iTextSelectionSuite) {
+		//CAlert::InformationAlert("Text is selected..");
+		InterfacePtr<const IIntegratorTarget> iIntegratorTarget_text(iTextSelectionSuite, UseDefaultIID());
+		if (iIntegratorTarget_text == nil)
+			return;
+		RangeData rangeData(0, RangeData::kLeanForward);
+		UIDRef textRef;
+		std::auto_ptr<IIntegratorTarget::TargetSuiteCollection> selectionSuites_text(iIntegratorTarget_text->GetTarget(ITextTarget::kDefaultIID));
+		for (IIntegratorTarget::TargetSuiteCollection::size_type i = 0; i < selectionSuites_text->size(); i++)
+		{
+			ITextTarget* target = (ITextTarget*)selectionSuites_text->at(i).get();
+			if (!target)
+				continue;
+
+			rangeData = target->GetRange();
+			
+			//int startIndex = rangeData.Start();
+			//int endIndex = rangeData->GetEnd();
+			//int length = rangeData.Length();
+
+			InterfacePtr<ITextModel>textModel(target->QueryTextModel());
+			
+			
+				//PMString textFrameContentLength = "selected content length-";
+				//textFrameContentLength.AppendNumber(textModel->TotalLength());
+			//	CAlert::InformationAlert(textFrameContentLength);
+			
+		}
+		int length = rangeData.Length();
+		PMString textFrameContentLength = "selected content length-";
+		textFrameContentLength.AppendNumber(length);
+		CAlert::InformationAlert(textFrameContentLength);
+
+	}
+	
 };
 
 void MyPlugin2SelectionObserver::HandleSelectionAttributeChanged(const ISelectionMessage* message) 
 {
-	CAlert::InformationAlert("Selection Attribute changed..");
+	//CAlert::InformationAlert("Selection Attribute changed..");
 };
 
 void MyPlugin2SelectionObserver::AutoAttach()
