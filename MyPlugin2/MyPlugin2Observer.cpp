f378569b27c2c5496c44d2c7f4a9f1f9ad3aac50
ganesh-inbetween <ganesh@inbetween.co.in>
2015-10-15 21:22:51 +0530

    DTP-1524 Poc check if style name has special sym.
    DTP-1524 #time 8h

--- MyPlugin2/MyPlugin2Observer.cpp
+++ MyPlugin2/MyPlugin2Observer.cpp
@@ -14,6 +14,14 @@
 #include "IWidgetParent.h"
 #include "IStringData.h"
 #include "IControlView.h"
+#include "widgetID.h"
+#include "CObserver.h"
+#include "ITextAttributes.h"
+#include "IUIDData.h"
+#include "UIDList.h"
+#include <boost\regex.hpp>
+#include <boost\regex.h>
+#include <string.h>
 
 CREATE_PMINTERFACE(MyPlugin2Observer, kMyPlugin2ObserverImpl);
 
@@ -79,20 +87,92 @@ void  MyPlugin2Observer::AutoDetach()
 void MyPlugin2Observer::Update(const ClassID& theChange, ISubject* theSubject,
 	const PMIID& protocol, void* changedBy)
 {
+	CAlert::InformationAlert("update called");
 	if (theChange == kCreateCharStyleCmdBoss)
 	{
 		CAlert::InformationAlert("Char Style Created");
+		InterfacePtr<ICommand> styleCmd((IPMUnknown*)changedBy, UseDefaultIID());
+		IDataBase* db = ::GetDataBase(theSubject);
+
+
+		if (styleCmd) {
+
+			InterfacePtr<IStyleInfo> cmdStyleInfo(styleCmd, UseDefaultIID());
+			InterfacePtr<IUIDData> styleUIDData(theSubject, UseDefaultIID());
+			if (cmdStyleInfo) {
+				
+				CAlert::InformationAlert("uid data found");
+				CAlert::InformationAlert(cmdStyleInfo->GetName());
+			}
+			else{
+				CAlert::InformationAlert("cmdStyleInfo nil");
+			}
+		}
+		else {
+			CAlert::InformationAlert("style cmd nil");
+		}
 	}
 	else if (theChange == kCreateParaStyleCmdBoss)
 	{
 		CAlert::InformationAlert("Para Style Created");
 	}
-	else if (theChange == kRenameStyleCmdBoss)
+	else if (theChange == kEditStyleGroupCmdBoss) {
+
+		CAlert::InformationAlert("style group edited");
+	} 
+	else if (theChange == kEditTextStyleCmdBoss) { // Style edited change occurred. 
+
+		CAlert::InformationAlert("style edited"); 
+		InterfacePtr<ICommand> styleCmd((IPMUnknown*)changedBy, UseDefaultIID()); //Get Command which changed the style.
+		InterfacePtr<IControlView> controlView(theSubject, UseDefaultIID());
+
+		if (styleCmd) { // If command found.
+
+			InterfacePtr<IStyleInfo> cmdStyleInfo(styleCmd, UseDefaultIID()); // Get style Information related to command.
+			InterfacePtr<IUIDData> styleUIDData(theSubject, UseDefaultIID());
+						
+			if (cmdStyleInfo) {   // If style Information found
+				PMString styleName = cmdStyleInfo->GetName();
+				styleName.StripWhiteSpace();
+				std::string currentStyle(styleName.GetPlatformString()); // convert style name to string type
+				boost::regex re("[^A-Za-z0-9_-]"); // regular ex to check special symbols.
+				bool result = regex_search(currentStyle, re); // check if style name contains special symbols.
+
+				if (result){ // If special symbol found.
+					CAlert::InformationAlert("Style Name contains special Symbols, hence will replace it by (_)" );
+					currentStyle = regex_replace(currentStyle, re, "_");
+					PMString newStylename;
+			    	WideString temp(currentStyle.c_str()); // widestring accept string of type Char * so convert it.
+					newStylename.Append(temp);
+					cmdStyleInfo->SetName(newStylename);
+					CAlert::InformationAlert(newStylename);
+				}
+				else{
+					CAlert::InformationAlert("special symbol not found");
+				}
+			}
+		}
+
+		
+	}
+	else if (theChange == kRenameStyleCmdBoss)    // Style renamed change occurred.
 	{
 		CAlert::InformationAlert("Style Renamed");
-		InterfacePtr<IControlView> view(theSubject, UseDefaultIID());
-		if (view && view->GetWidgetID() == kPreferencesDialogWidgetID)
-			CAlert::InformationAlert("aet");
+		InterfacePtr<ICommand> styleCmd((IPMUnknown*)changedBy, UseDefaultIID());
+		IDataBase* db = ::GetDataBase(theSubject);
+	  
+		if (styleCmd) {
+			InterfacePtr<IStyleInfo> cmdStyleInfo(styleCmd, UseDefaultIID());
+			CAlert::InformationAlert("style cmd found");
+			if (cmdStyleInfo) {
+			   CAlert::InformationAlert("uid data found");
+		    }
+		   else{
+			   CAlert::InformationAlert("cmdStyleInfo nil");
+		    }
+		}
+		else {
+			CAlert::InformationAlert("style cmd nil");
+		}
 	}
-
 }
